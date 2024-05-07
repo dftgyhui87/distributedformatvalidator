093255cc3cc365324deccea8a51591454dffc189
dftgyhui87 <gs1goldoska@hotmail.com>
2024-05-07 18:27:28 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
     }
   }
-  return arr;
+  return dp[amount] === Infinity ? -1 : dp[amount];
 }
